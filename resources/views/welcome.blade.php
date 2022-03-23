f450668f45ae93dcda79036611027518430a117e
shunsuke.konno <ubuntu@ip-172-31-35-248.ap-northeast-1.compute.internal>
2022-03-23 01:55:58 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>旅と想いで未来を創る。</h1>
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'さあ、あなただけの表紙を作りましょう。!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+     @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>旅と想いで未来を創る。</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'さあ、あなただけの表紙を作りましょう。!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
